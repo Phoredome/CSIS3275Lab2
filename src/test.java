22dd2343a1a252cb4b811d6f2b9ada68120d08a1
Bosco <mario1997winfree@hotmail.com>
2019-05-29 11:17:15 -0700

    Test Edit

--- src/test.java
+++ src/test.java
@@ -1,9 +1,11 @@
 
-public class test {
+public class test 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!");
 	}
 
 }
